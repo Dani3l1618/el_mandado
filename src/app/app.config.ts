b0c7291c9eeb37492f843d37512bce7696c7cd13
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2024-11-18 14:49:38 -0600

    Add list shop footer component with budget and total display

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,27 +1,26 @@
-import { DEFAULT_CURRENCY_CODE, EnvironmentProviders, LOCALE_ID, Provider } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeMX from '@angular/common/locales/es-MX';
+import { EnvironmentProviders, LOCALE_ID, Provider } from '@angular/core';
 import {
+  PreloadAllModules,
   RouteReuseStrategy,
   provideRouter,
   withPreloading,
-  PreloadAllModules,
 } from '@angular/router';
-import { routes } from './app.routes';
 import {
   IonicRouteStrategy,
   provideIonicAngular,
 } from '@ionic/angular/standalone';
-import {registerLocaleData} from "@angular/common"
-import localeMX from "@angular/common/locales/es-MX";
+import { routes } from './app.routes';
 
-registerLocaleData(localeMX, "es-Mx");
+registerLocaleData(localeMX, 'es-Mx');
 
 const APP_CONFIG: (Provider | EnvironmentProviders)[] = [
   { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   provideIonicAngular(),
   provideRouter(routes, withPreloading(PreloadAllModules)),
-  {provide: LOCALE_ID, useValue: "es-Mx"},
+  { provide: LOCALE_ID, useValue: 'es-Mx' },
   // {provide: DEFAULT_CURRENCY_CODE, useValue: 'en-US' }
-
 ];
 
 export default APP_CONFIG;
---
+++ src/app/pages/list-shop/components/list-shop-footer/list-shop-footer.component.ts
@@ -0,0 +1,45 @@
+import { CurrencyPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
+import { IonFooter, IonText, IonToolbar } from '@ionic/angular/standalone';
+import { ListShopService } from '../../services/list-shop.service';
+
+const imports = [IonFooter, IonToolbar, IonText, CurrencyPipe];
+
+@Component({
+  selector: 'app-list-shop-footer',
+  templateUrl: './list-shop-footer.component.html',
+  styleUrls: ['./list-shop-footer.component.scss'],
+  standalone: true,
+  imports,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListShopFooterComponent {
+  private listShopService = inject(ListShopService);
+  private totalColor = [
+    { max: 85, color: 'white' },
+    { max: 100, color: 'warning' },
+  ];
+
+  budget = computed(() => this.listShopService.storeConfig()?.budget ?? 0.001);
+
+  rest = computed(() => this.budget() - this.total());
+
+  total = signal(0); // topar el valor a 9999
+
+  colorTotal = computed(() => {
+    const porcent = (this.total() / this.budget()) * 100;
+    return this.getColor(porcent);
+  });
+
+  private getColor(porcent: number): string {
+    const color = this.totalColor.find((c) => porcent <= c.max);
+
+    return color?.color ?? 'danger';
+  }
+}
