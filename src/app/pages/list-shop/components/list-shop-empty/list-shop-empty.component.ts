7495dbfa99732aa3177d8285bb07e8ba0779a512
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2024-11-19 08:13:18 -0600

    Add list shop empty component with animation and styling

---
+++ src/app/pages/list-shop/components/list-shop-empty/list-shop-empty.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { IonImg, IonText } from '@ionic/angular/standalone';
+
+const imports = [IonImg, IonText];
+@Component({
+  selector: 'app-list-shop-empty',
+  templateUrl: './list-shop-empty.component.html',
+  styleUrls: ['./list-shop-empty.component.scss'],
+  standalone: true,
+  imports,
+})
+export class ListShopEmptyComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
