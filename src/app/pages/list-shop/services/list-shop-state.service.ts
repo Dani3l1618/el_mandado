613d35faecbfc1771650e9fdd916c73500559b5b
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2024-12-09 08:51:31 -0600

    Factorización en servicio de estados de lista de compras.

--- src/app/pages/list-shop/services/list-shop-state.service.ts
+++ src/app/pages/list-shop/services/list-shop-state.service.ts
@@ -9,11 +9,14 @@ import {
 @Injectable()
 export class ListShopStateService {
   mode = signal<'new' | 'draft'>('new');
-  timeInStore = signal(0);
+
   stores = signal<Store[]>([]);
   storeConfig = signal<ListShopConfig | undefined>(undefined);
+
   listItemShop = signal<ListShopItem[]>([]);
+  timeInStore = signal(0);
   listShopTotal = computed(() => this.totalListShop(this.listItemShop()));
+
   listDrafts = signal<ListShop[]>([]);
   currentDraft = signal<ListShop | null>(null);
   listSaved = computed(() => this.currentDraft() !== null);
@@ -22,7 +25,7 @@ export class ListShopStateService {
     return listShop.reduce((acc, item) => acc + item.price * item.quantity, 0);
   }
 
-  reset() {
+  public reset() {
     this.storeConfig.set(undefined);
     this.stores.set([]);
     this.listItemShop.set([]);
