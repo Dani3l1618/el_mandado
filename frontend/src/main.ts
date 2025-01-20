89f1e1dd7d1d30afc2cd829d31364d0b9a0cb467
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2025-01-20 09:29:55 -0600

    Add error handling to application bootstrap process

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
-import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import APP_CONFIG from './app/app.config';
 
 bootstrapApplication(AppComponent, {
   providers: APP_CONFIG,
+}).catch((error) => {
+  console.log('[APP CONSOLE] ERROR: ', error);
 });
