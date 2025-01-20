e9c1b29641733ab020591e0ae71a71554617dcc5
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2025-01-20 17:04:52 -0600

    Refactor MongoDB connection configuration in .env and index.ts; separate user, password, and host variables for clarity

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,11 @@ import App from './app';
 
 dotenv.config();
 const APP_PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
 const MONGO_DB = process.env.MONGO_DB;
-const mongoUri = `${MONGO_URI}/${MONGO_DB}`;
+const MONGO_HOST = process.env.MONGO_HOST;
+const mongoUri = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}/${MONGO_DB}`;
 
 const app = new App(mongoUri).app;
 
