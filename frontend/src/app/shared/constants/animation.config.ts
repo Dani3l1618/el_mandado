61ab48d356e77c9a76ea5b80f721df8f39bcca94
Daniel Aguilar <dniel.aguilarojeda@gmail.com>
2025-02-11 08:51:52 -0600

    Add clock animation to animation configuration; include em_clock.json

--- frontend/src/app/shared/constants/animation.config.ts
+++ frontend/src/app/shared/constants/animation.config.ts
@@ -1,6 +1,6 @@
 const path = "/assets/animations/";
 
 export const ANIMATION_SOURCE = {
-  "bascket": `${path}em_empty.json`
-
+  bascket: `${path}em_empty.json`,
+  clock: `${path}em_clock.json`,
 };
